2dda10d4074a7123164b84219c3da0f4d6c02482
matthewGrim <rohan.ramasamy@gmail.com>
2018-05-02 18:13:31 +0200

    #2 - Minor changes in peer review

--- 001-DVP-BeamSpreading/Beam_Divergence_Brandhorst_Verification.py
+++ 001-DVP-BeamSpreading/Beam_Divergence_Brandhorst_Verification.py
@@ -10,34 +10,34 @@ import numpy as np
 # Altitudes
 perigee = 5e5
 apogee = 3e7
-altitude = np.asarray([500000.0, 30000000.0])
+altitude = np.asarray([perigee, apogee])
 
 # Transmitter parameters, based on Brandhorst
 trans_area = 4.0
 trans_power = 40000.0
 wavelength = 850e-9
-trans_beam_radius = np.sqrt(trans_area/np.pi)
+trans_beam_radius = np.sqrt(trans_area / np.pi)
 
 # Surface beam parameters, based on Gaussian beam divergence
-surf_beam_radius = trans_beam_radius * np.sqrt(1 + (altitude * wavelength / trans_area)**2)
+surf_beam_radius = trans_beam_radius * np.sqrt(1 + (altitude * wavelength / trans_area) ** 2)
 surf_beam_area = np.pi * (surf_beam_radius ** 2)
 
 print(surf_beam_area)
 
 # Brandhorst claims that at maximum surface beam size (assumed to occur at apogee)
 # the beam covers only 60% of the receiver area
-rec_area = (1/0.6)*surf_beam_area
+rec_area = (1 / 0.6) * surf_beam_area
 
 print(rec_area.item(1))
 
 # The flux at the surface of the moon
-surf_flux = np.divide(trans_power, surf_beam_area)
+surf_flux = trans_power / surf_beam_area
 
 # AM0 solar irradiance
 sun = 1377  # W/m2
 
 # Comparing laser beam flux to solar irradiance to verify "laser beaming results" in Brandhorst
-relative_flux = np.divide(surf_flux, sun)
+relative_flux = surf_flux / sun
 
 print(relative_flux)
 
--- 001-DVP-BeamSpreading/Rover_Power.py
+++ 001-DVP-BeamSpreading/Rover_Power.py
@@ -21,18 +21,18 @@ altitude = np.arange(500000.0, 30000000.0, 50000.0)  # 500 to 30000 km altitude
 
 # Transmitter parameters
 trans_power = np.arange(1000.0, 100000.0, 100.0)  # 10 to 40 kW transmitter power
-laser_wavelength = 850.0*10**(-9)  # For GaAs cells
-microwave_wavelength = 122.0*10**(-6)  # For 2.45 GHz microwave
+laser_wavelength = 850e-9  # For GaAs cells
+microwave_wavelength = 122e-6  # For 2.45 GHz microwave
 trans_radius = np.arange(0.001, 0.1, 0.0001)
-trans_area = np.pi*(trans_radius**2)
+trans_area = np.pi * trans_radius ** 2
 
 # Surface beam parameters
 surf_power = 10.0
 # surface beam radius relative to transmitter beam radius, according to conservation of flux
-relative_beam_radius = np.sqrt(trans_power/surf_power)
+relative_beam_radius = np.sqrt(trans_power / surf_power)
 
 plt.figure(1)
-plt.plot(trans_power/1000.0, relative_beam_radius)
+plt.plot(trans_power / 1000.0, relative_beam_radius)
 plt.ylabel('Surface Beam Radius (Normalized by Transmitter Radius)')
 plt.xlabel('Transmitter Power [kW]')
 plt.title('Beam Divergence for 10 W/m2 Surface Flux')
@@ -45,18 +45,18 @@ plt.show()
 # Satellite orbit
 # For a desired surface beam size relative to transmitter size, what altitude is required?
 # Chosen beam radius ratio is 1e6 --> 1 mm goes to 1 km, 10 cm goes to 100 km
-desired_beam_div = 1000000.0
-z_laser = trans_area*np.sqrt(desired_beam_div**2 - 1)/laser_wavelength
-z_microwave = trans_area*np.sqrt(desired_beam_div**2 - 1)/microwave_wavelength
+desired_beam_div = 1e6
+z_laser = trans_area * np.sqrt(desired_beam_div ** 2 - 1) / laser_wavelength
+z_microwave = trans_area * np.sqrt(desired_beam_div ** 2 - 1) / microwave_wavelength
 
 # What is the resulting surface beam flux, based on a 10 MW transmitter?
 # 10 kW transmitter results in a flux << 1W/m^2
 surf_radius = trans_radius * desired_beam_div
-surf_flux = 100000000.0/(np.pi * surf_radius**2)
+surf_flux = 1e8 / (np.pi * surf_radius ** 2)
 
 plt.figure(2)
-laser, = plt.plot(np.log10(trans_radius), z_laser/1000.0, 'r--', label='Laser 850 nm')
-microwave, = plt.plot(np.log10(trans_radius), z_microwave/1000.0, 'b--', label='Microwave 122 um')
+laser, = plt.plot(np.log10(trans_radius), z_laser / 1000.0, 'r--', label='Laser 850 nm')
+microwave, = plt.plot(np.log10(trans_radius), z_microwave / 1000.0, 'b--', label='Microwave 122 um')
 plt.ylabel('Required Altitude [km]')
 plt.xlabel('Logarithm of Transmitter Radius')
 plt.title('Altitude Required for Surface Beam Radius 1000000X Larger Than Transmitter')
