dc0cf9239e1da7e8bbdab39cbc5c9840f8ea5d66
Darian van Paridan <dvanpar@gmail.com>
2018-05-11 12:51:55 +0200

    #5 Uploaded script and .csv files for verifying ideal apogee altitude for SPS orbit and 45N lunar target

---
+++ 002-DVP-BrandhorstStudy/replicate_brandhorst_fig_3.py
@@ -0,0 +1,107 @@
+"""
+Author: Rohan Ramasamy
+Date: 04/03/2018
+
+This is a data parsing script for reeding in a csv with access times from the lunar sps study. This particular analysis
+compares against the results in figure 2 of:
+
+"A solar electric propulsion mission for lunar power beaming" - H. W. Brandhorst
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+from general_functions import *
+
+
+def compute_access_to_target(access_fname, target_illumination_fname, satellite_illumination_fname, sim_start_time,
+                             **kwargs):
+    """
+    Get the access times between a target and satellite subject to a set of constraints, concerning illumination.
+    Access points when the target can be illuminated are excluded. Access points when the satellite is not
+    illuminated can also be excluded.
+
+    :param access_fname: file containing access times between satellite and target
+    :param target_illumination_fname: file containing illumination times of target
+    :param satellite_illumination_fname: file containing illumination times of satellite
+    :param sim_start_time: start time of simulation
+    :return:
+    """
+    access_times = get_access_times(access_fname)
+    check_event_order_consistency(access_times)
+
+    satellite_illumination_times = get_illumination_event_times_from_file(satellite_illumination_fname)
+    check_event_order_consistency(satellite_illumination_times)
+    target_illumination_times = get_illumination_event_times_from_file(target_illumination_fname, invert=False)
+    check_event_order_consistency(target_illumination_times)
+    target_eclipse_times = get_illumination_event_times_from_file(target_illumination_fname, invert=True)
+    check_event_order_consistency(target_eclipse_times)
+
+    # Filter out times when satellite is not illuminated
+    filter_satellite_illumination = kwargs.pop("filter_satellite_illumination", False)
+    if filter_satellite_illumination:
+        access_times = get_overlap_between_events(access_times, satellite_illumination_times)
+    check_event_order_consistency(access_times)
+
+    # Get times when target power is available
+    target_illumination_times = combine_events(access_times, target_illumination_times)
+    check_event_order_consistency(target_illumination_times)
+
+    # Filter out times when power is not needed by target
+    filter_target_eclipses = kwargs.pop("filter_target_illumination", False)
+    if filter_target_eclipses:
+        access_times = get_overlap_between_events(access_times, target_eclipse_times)
+    check_event_order_consistency(access_times)
+
+    access_durations = list()
+    for i, access_start in enumerate(access_times["Start"]):
+        access_end = access_times["End"][i]
+        access_duration = (access_end - access_start).total_seconds()
+        access_durations.append(access_duration)
+    last_eclipse_start = sim_start_time
+    eclipse_durations = list()
+    for j, target_power_start in enumerate(target_illumination_times["Start"]):
+        target_power_end = target_illumination_times["End"][j]
+        eclipse_time = (target_power_start - last_eclipse_start).total_seconds()
+        eclipse_durations.append(eclipse_time)
+        last_eclipse_start = target_power_end
+
+    return np.sum(np.asarray(access_durations)), np.sum(np.asarray(eclipse_durations))
+
+
+def perigee_angle_scan():
+    """
+    Function to replicate the results from Branhorst paper
+    """
+    # degrees = np.asarray([0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285,
+    #                       300, 315, 330, 345])
+    degrees = np.asarray([0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330])
+    start = convert_string_to_datetime(['2008', '07', '01', '11', '0', '0.0'])
+    total_access_times = np.zeros(degrees.shape)
+    total_eclipse_times = np.zeros(degrees.shape)
+
+    for i, degree in enumerate(degrees):
+        access_fname = "{}-degrees.csv".format(degree)
+        illumination_fname = "illumination_times.csv"
+        sat_illumination_fname = "{}-sat-illumination_times.csv".format(degree)
+
+        total_access, total_eclipse = compute_access_to_target(access_fname, illumination_fname, sat_illumination_fname,
+                                                               start, filter_satellite_illumination=False,
+                                                               filter_target_illumination=False)
+
+        total_access_times[i] = total_access / 3600 / 2
+        total_eclipse_times[i] = total_eclipse / 3600 / 2
+
+    plt.figure()
+    plt.plot(degrees, total_access_times)
+    plt.scatter(degrees, total_access_times)
+    plt.xlabel('Relative Argument of Pariapsis')
+    plt.ylabel('Total Access Time [hrs]')
+    plt.show()
+
+
+if __name__ == '__main__':
+    perigee_angle_scan()
+
---
+++ 003-DVP-SPS_Orbit_Study/SPS_Optimal_Orbit_Verification.py
@@ -0,0 +1,50 @@
+""""
+11/05/2018
+Author: Darian van Paridon
+
+This script is for verifying the optimal apogee altitude for a lunar SPS mission, which Brandhorst claims
+is 30000 km in the paper "A solar electric propulsion mission for lunar power beaming"
+
+"""
+
+import matplotlib.pyplot as plt
+from DVP_general_SPS_functions import *
+from general_functions import convert_string_to_datetime
+
+
+def scan_apogee_altitudes(apogees, start):
+    # This function analyzes the availability of the SPS to a lunar target at 45 N as a function of the
+    # altitude of the orbital apogee
+
+    total_active_time = np.zeros(len(apogees))
+
+    raw_eclipse_target = 'Target1-Eclipse-Edited.csv'
+    eclipse_target = parse_csv_to_array(raw_eclipse_target, start)
+
+    for i in range(len(apogees)):
+        raw_sunlight = "Lighting({}k).csv".format(apogees[i])
+        raw_access = "Access({}k).csv".format(apogees[i])
+
+        access_times = parse_csv_to_array(raw_access, start)
+        sunlight_times = parse_csv_to_array(raw_sunlight, start)
+        print('\n')
+        print('Apogee altitude {}000 km'.format(apogees[i]))
+        total_active_time[i] = determine_SPS_active_time(sunlight_times, eclipse_target, access_times)
+
+    return total_active_time
+
+
+def main():
+
+    start = convert_string_to_datetime(['2008', '07', '01', '10', '0', '0.0'])
+    apogees = (1, 2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55)
+    active_times = scan_apogee_altitudes(apogees, start)
+    apogees_scaled = [i * 1000 for i in apogees]
+    active_times_scaled = [i / 3600.0 for i in active_times]
+    plt.plot(apogees_scaled, active_times_scaled)
+    plt.xlabel('Apogee Altitude [km]')
+    plt.ylabel('Hours of Access (Over 2 Years)')
+    plt.show()
+
+
+main()
