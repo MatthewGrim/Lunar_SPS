b346de36f3ecf7bbd0fcb95fb9dc2cf29878a03d
Darian van Paridan <dvanpar@gmail.com>
2018-05-15 16:02:09 +0200

    #4 Updated script to account for all laser beaming results presented in the Brandhorst paper.

--- 004-DVP-SPS_Beaming_Study/Verify_Brandhorst_BeamRanging.py
+++ 004-DVP-SPS_Beaming_Study/Verify_Brandhorst_BeamRanging.py
@@ -39,12 +39,68 @@ def import_range_data(file_name, sim_start):
     return parsed_data
 
 
-start = convert_string_to_datetime(['2008', '07', '01', '10', '0', '0.0'])
-end = convert_string_to_datetime(['2010', '06', '30', '10', '0', '0.0'])
-total_duration = (end - start).total_seconds()
-raw_range_data = 'SPS-to-Target-Range.csv'
-range_data = import_range_data(raw_range_data, start)
+def determine_surface_beam_data(range_data):
 
-plt.figure(1)
-plt.plot(range_data[0] / 86400.0, range_data[1])
-plt.show()
+    # Transmitter parameters used in Brandhorst study
+    wavelength = 850e-9
+    trans_area = 4
+    trans_power = 40e3
+
+    # Receiver parameters used in Brandhorst study
+    pwr_area_metric = 300
+    total_rec_pwr = 60e3
+    rec_area = total_rec_pwr / pwr_area_metric
+    # Efficiency assumed in Brandhorst study
+    solar_cell_eff = 0.45
+
+    # General AM0 solar irradiance
+    solar_flux = 1367
+
+    # Gaussian beam divergence to determine size of beam at the target
+    trans_radius = np.sqrt(trans_area / np.pi)
+    surf_beam_radius = trans_radius * np.sqrt(1 + (wavelength * (range_data[1] * 1000.0) / (np.pi * trans_radius ** 2)) ** 2)
+    surf_beam_area = np.pi * surf_beam_radius ** 2
+
+    # Determine surface flux relative to solar flux for comparison to Brandhorst
+    surf_flux = (trans_power / (np.pi * surf_beam_radius ** 2))
+    # Determine percentage of the total receiver area which is covered for comparison to Brandhorst
+    percent_rec_covered = (surf_beam_area / rec_area) * 100
+    # Determine total power delivered to receiver
+    power_delivered = surf_beam_area * surf_flux * solar_cell_eff
+
+    return (surf_flux / solar_flux), percent_rec_covered, power_delivered
+
+
+def main():
+
+    start = convert_string_to_datetime(['2008', '07', '01', '10', '0', '0.0'])
+    raw_range_data = 'SPS-to-Target-Range.csv'
+    range_data = import_range_data(raw_range_data, start)
+    surf_flux, percent_rec_covered, power_delivered = determine_surface_beam_data(range_data)
+
+    range_days = range_data[0] / 86400.0
+
+    # Brandhorst figures begin on the 414th (7:55 am) day of the simulation, and end on the 416th day (7:55 am)
+    brandhorst_range = range_days[(range_days >= 414.0) & (range_days <= 416.0)]
+
+    print(range_days)
+    print(brandhorst_range)
+    plt.figure(1)
+    plt.subplot(221)
+    plt.scatter(range_days, range_data[1], s=0.1)
+    plt.ylabel('Distance to Target [km]')
+    plt.subplot(222)
+    plt.scatter(range_days, surf_flux,s=0.1)
+    plt.ylabel('Surface Beam Flux [AM0]')
+    plt.subplot(223)
+    plt.scatter(range_days, percent_rec_covered,s=0.1)
+    plt.xlabel('Days Since Simulation Start')
+    plt.ylabel('Percentage of Receiver Covered')
+    plt.subplot(224)
+    plt.scatter(range_days, power_delivered / 1000.0, s=0.1)
+    plt.xlabel('Days Since Simulation Start')
+    plt.ylabel('Power Delivered at Receiver [kW]')
+    plt.show()
+
+
+main()
