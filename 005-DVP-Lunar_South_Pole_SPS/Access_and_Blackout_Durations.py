178ec09626e4b4bfd526b0ee62d0bf4e59f95439
Darian van Paridan <dvanpar@gmail.com>
2018-05-25 11:14:16 +0200

    #7 Uploaded additional data files for scenarios which incorporate lunar surface terrain.

--- 005-DVP-Lunar_South_Pole_SPS/Access_and_Blackout_Durations.py
+++ 005-DVP-Lunar_South_Pole_SPS/Access_and_Blackout_Durations.py
@@ -11,9 +11,12 @@ from DVP_general_SPS_functions import *
 
 def main():
     # Import data and set the start and end times of the simulation
-    raw_sunlight_sps = "SPS(4k)_Lighting.csv"
-    raw_access_sps = "SPS(4k)_Access.csv"
-    raw_sunlight_target = 'Target_Lighting.csv'
+    raw_sunlight_sps = "SPS(4.0k)_Lighting.csv"
+    raw_access_sps = "SPS(4.0k)_Access.csv"
+    raw_sunlight_target = 'Target_Lighting_89.45-222.69.csv'
+    raw_sunlight_target_terrain = 'Target_Lighting_Terrain_89.45-222.69.csv'
+    raw_sunlight_target_terrain_elevated = 'Target_Lighting_Terrain_Elevated_89.45-222.69.csv'
+    raw_sunlight_target_terrain_v2 = 'Target_Lighting_Terrain_89.45-222.69_HannaTerrain.csv'
     start = convert_string_to_datetime(['2018', '05', '17', '10', '0', '0.0'])
     end = convert_string_to_datetime(['2020', '05', '17', '10', '0', '0.0'])
     total_duration = (end - start).total_seconds()
@@ -30,6 +33,16 @@ def main():
     sunlight_target = parse_csv_to_array(raw_sunlight_target, start)
     eclipse_target = invert_events_list(sunlight_target, total_duration)
 
+    sunlight_target_terrain = parse_csv_to_array(raw_sunlight_target_terrain, start)
+    eclipse_target_terrain = invert_events_list(sunlight_target_terrain, total_duration)
+
+    sunlight_target_terrain_elevated = parse_csv_to_array(raw_sunlight_target_terrain_elevated, start)
+    eclipse_target_terrain_elevated = invert_events_list(sunlight_target_terrain_elevated, total_duration)
+
+    sunlight_target_terrain_v2 = parse_csv_to_array(raw_sunlight_target_terrain_v2, start)
+    eclipse_target_terrain_v2 = invert_events_list(sunlight_target_terrain_v2, total_duration)
+
+
     # Calculates the total active time for SPS, based on target access
     # and eclipses, as well as satellite illumination times
     print("\n")
@@ -37,13 +50,38 @@ def main():
     sps_active = determine_SPS_active_time(sps_sunlit, eclipse_target, sps_access)
     sps_blackout = determine_blackout_data(sps_active, eclipse_target, total_duration)
 
-    sps_days = [i / 86400.0 for i in sps_blackout[0]]
-    sps_dur = [i / 3600.0 for i in sps_blackout[2]]
+    es = [i / 86400.0 for i in eclipse_target[0]]
+    ed = [i / 3600.0 for i in eclipse_target[2]]
+    ets = [i / 86400.0 for i in eclipse_target_terrain[0]]
+    etd = [i / 3600.0 for i in eclipse_target_terrain[2]]
+    etes = [i / 86400.0 for i in eclipse_target_terrain_elevated[0]]
+    eted = [i / 3600.0 for i in eclipse_target_terrain_elevated[2]]
+    et2s = [i / 86400.0 for i in eclipse_target_terrain_v2[0]]
+    et2d = [i / 3600.0 for i in eclipse_target_terrain_v2[2]]
+
+
+    print("Max eclipse w/o terrain = {}".format(max(ed)))
+    print("Max eclipse w terrain = {}".format(max(etd)))
+    print("Max eclipse w terrain and elevation = {}".format(max(eted)))
+    print("Max eclipse w terrain from Rohan = {}".format(max(et2d)))
 
     plt.figure(1)
-    plt.bar(sps_days, sps_dur)
-    plt.title('Blackout Durations - Brandhorst Configuration')
+    plt.subplot(411)
+    plt.bar(es, ed)
     plt.ylabel('Duration [h]')
+    plt.xlim([0, 718])
+    plt.subplot(412)
+    plt.bar(ets, etd)
+    plt.ylabel('Duration [h]')
+    plt.xlim([0, 718])
+    plt.subplot(413)
+    plt.bar(etes, eted)
+    plt.ylabel('Duration [h]')
+    plt.xlim([0, 718])
+    plt.subplot(414)
+    plt.bar(et2s, et2d)
+    plt.ylabel('Duration [h]')
+    plt.xlim([0, 718])
     plt.show()
 
 
--- 005-DVP-Lunar_South_Pole_SPS/SouthPole_AccessTimes.py
+++ 005-DVP-Lunar_South_Pole_SPS/SouthPole_AccessTimes.py
@@ -12,7 +12,7 @@ from DVP_general_SPS_functions import *
 import matplotlib.pyplot as plt
 
 
-def scan_apogee_altitude(start, total_duration, scan_array):
+def scan_apogee(start, total_duration, scan_array):
     # This function is used to scan through a series of .csv files from STK to analyze
     # the coverage availability for various arguments of perigee
     total_active_time = np.zeros(len(scan_array))
@@ -33,16 +33,56 @@ def scan_apogee_altitude(start, total_duration, scan_array):
 
         print('\n')
         print('Altitude {}000 km'.format(scan_array[i]))
-        total_active_time[i] = determine_SPS_active_time(sunlight_times, eclipse_times, access_times)
-
-        max_blackout_time[i] = determine_blackout_data(active_times, eclipse_times, total_duration)
-
+        activity = determine_SPS_active_time(sunlight_times, eclipse_times, access_times)
+        total_active_time[i] = np.sum(activity[2])
+        blacktivity = determine_blackout_data(active_times, eclipse_times, total_duration)
+        max_blackout_time[i] = max(blacktivity[2])
     # Plot which shows the variation in total access time
     # as a function of relative argument of perigee between SPS2 and SPS1
     plt.figure(1)
     plt.subplot(211)
     plt.plot(scan_array * 1000.0, total_active_time / 3600.0)
-    plt.xlabel('Altitude [km]')
+    plt.ylabel('Total Active Time [hrs]')
+    plt.title('Fixed Perigee 300 km')
+    plt.subplot(212)
+    plt.plot(scan_array * 1000.0, max_blackout_time / 3600.0)
+    plt.xlabel('Apogee Altitude [km]')
+    plt.ylabel('Maximum Blackout Duration [hrs]')
+    plt.show()
+
+    return scan_array, total_active_time, total_blackout_time
+
+
+def scan_altitude(start, total_duration, scan_array):
+    # This function is used to scan through a series of .csv files from STK to analyze
+    # the coverage availability for various arguments of perigee
+    total_active_time = np.zeros(len(scan_array))
+    total_blackout_time = np.zeros(len(scan_array))
+    max_blackout_time = np.zeros(len(scan_array))
+    raw_target_lighting = "Target_Lighting.csv"
+    target_lighting = parse_csv_to_array(raw_target_lighting, start)
+    eclipse_times = invert_events_list(target_lighting, total_duration)
+    for i in range(len(scan_array)):
+        raw_sunlight = "SPS({}k)_Lighting.csv".format(scan_array[i])
+        raw_access = "SPS({}k)_Access.csv".format(scan_array[i])
+
+        access_times = parse_csv_to_array(raw_access, start)
+        sunlight_times = parse_csv_to_array(raw_sunlight, start)
+
+        sps_access_sunlit_times = get_event_overlaps(sunlight_times, access_times)
+        active_times = get_event_overlaps(sps_access_sunlit_times, eclipse_times)
+
+        print('\n')
+        print('Altitude {}000 km'.format(scan_array[i]*1000))
+        activity = determine_SPS_active_time(sunlight_times, eclipse_times, access_times)
+        total_active_time[i] = np.sum(activity[2])
+        blacktivity = determine_blackout_data(active_times, eclipse_times, total_duration)
+        max_blackout_time[i] = max(blacktivity[2])
+    # Plot which shows the variation in total access time
+    # as a function of relative argument of perigee between SPS2 and SPS1
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(scan_array * 1000.0, total_active_time / 3600.0)
     plt.ylabel('Total Active Time [hrs]')
     plt.title('Circular Orbits')
     plt.subplot(212)
@@ -55,13 +95,14 @@ def scan_apogee_altitude(start, total_duration, scan_array):
 
 
 def main():
-    start = convert_string_to_datetime(['2018', '05', '17', '10', '0', '0.0'])
-    end = convert_string_to_datetime(['2020', '05', '17', '10', '0', '0.0'])
+    start = convert_string_to_datetime(['2018', '05', '18', '10', '0', '0.0'])
+    end = convert_string_to_datetime(['2020', '05', '18', '10', '0', '0.0'])
     total_duration = (end - start).total_seconds()
 
-    altitudes = np.asarray([2, 3, 4, 5, 6, 8, 10])
+    altitudes = np.asarray([1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10])
     apogees = np.array([5, 6, 7, 8, 10, 15, 20])
-    scan_apogee_altitude(start, total_duration, apogees)
+    # scan_apogee(start, total_duration, apogees)
+    scan_altitude(start, total_duration, altitudes)
     # sps_access_raw = 'SPS(3k)_Access_89.45-222.69.csv'
     # sps_lighting_raw = 'SPS(3k)_Lighting_89.45-222.69.csv'
     # target_lighting_raw = 'Target_Lighting_89.45-222.69.csv'
@@ -69,14 +110,25 @@ def main():
     # sps_lighting = parse_csv_to_array(sps_lighting_raw, start)
     # target_lighting = parse_csv_to_array(target_lighting_raw, start)
     # target_eclipse = invert_events_list(target_lighting, total_duration)
-
+    #
     # print("\n")
     # print("For a 3000 km circular orbit:")
     # sps_active_circular_orbit = determine_SPS_active_time(sps_lighting, target_eclipse, sps_access)
     # print('Maximum eclipse duration without SPS is {} hours'.format(round(max(target_eclipse[2]) / 3600.0, 2)))
     # sps_sunny_access_times = get_event_overlaps(sps_lighting, sps_access)
     # active_times_circle_orbit = get_event_overlaps(sps_sunny_access_times, target_eclipse)
-    # determine_blackout_data(active_times_circle_orbit, target_eclipse, total_duration)
+    # blacktivity = determine_blackout_data(active_times_circle_orbit, target_eclipse, total_duration)
+    #
+    # bs = [i / 86400.0 for i in blacktivity[0]]
+    # bd = [i / 3600.0 for i in blacktivity[2]]
+    # es = [i / 86400.0 for i in target_eclipse[0]]
+    # ed = [i / 3600.0 for i in target_eclipse[2]]
+    #
+    # plt.subplot(211)
+    # plt.bar(bs, bd)
+    # plt.subplot(212)
+    # plt.bar(es, ed)
+    # plt.show()
 
 
 main()
