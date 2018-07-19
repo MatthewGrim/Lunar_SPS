8e38a6f688a226d3621413517b2e563d85bf128b
Darian van Paridan <dvanpar@gmail.com>
2018-07-19 11:46:01 +0200

    #20 Changed scripts to work on processed data availble in github as opposed to raw local data.

--- 005-DVP-Lunar_South_Pole_SPS/Check_Gradient_in_Blackout_Events.py
+++ 005-DVP-Lunar_South_Pole_SPS/Check_Gradient_in_Blackout_Events.py
@@ -8,7 +8,7 @@ to the mean blackout durations as a function of the perigee altitude of an orbit
 """
 
 from DVP_general_SPS_functions import *
-from DVP_Programmatic_Functions import vary_orbital_elements_incrementing_resolution
+from DVP_Programmatic_Functions import vary_orbital_elements_incrementing_resolution, read_data_from_file, sort_incremented_resolution_data
 import os
 
 
@@ -25,51 +25,47 @@ def main():
     end = convert_string_to_datetime(['2020', '05', '17', '10', '0', '0.0'])
     total_duration = (end - start).total_seconds()
 
-    # Set limit for orbit size to select out of data set which is contains orbits up to 5000 km altitude.
-    max_perigee = 1700.0
-    max_apogee = 1700.0
+    # Select perigee altitude along which to check gradients in blackout event durations
+    perigee = 40.0
 
     # Get orbit data set
-    semi_maj_axis, eccentricity, orbit_data = vary_orbital_elements_incrementing_resolution(max_perigee, max_apogee)
+    semi_maj_axis, eccentricity, orbit_data = vary_orbital_elements_incrementing_resolution(5000.0, 5000.0)
 
     # Find unique perigees and apogees tested for plotting
-    unique_perigees = [orbit_data[1][0]]
-    unique_apogees = [orbit_data[1][1]]
     r_moon = 1737.0
+    perigee_altitudes = [orbit_data[1][0] - r_moon]
+    apogee_altitudes = [orbit_data[1][1] - r_moon]
     for i in range(1, len(orbit_data)):
-        if orbit_data[i][0] > max(unique_perigees):
-            unique_perigees.append(orbit_data[i][0])
-        if orbit_data[i][1] > max(unique_apogees):
-            unique_apogees.append(orbit_data[i][1])
+        if (orbit_data[i][0] - r_moon) > max(perigee_altitudes):
+            perigee_altitudes.append(orbit_data[i][0] - r_moon)
+        if (orbit_data[i][1] - r_moon) > max(apogee_altitudes):
+            apogee_altitudes.append(orbit_data[i][1] - r_moon)
 
-    # Initialize data sets
-    max_blackouts = []
-    mean_blackouts = []
+    # Import blackout event data for this study
+    data_set = {}
+    data_set['total_blackout_time'] = sort_incremented_resolution_data(orbit_data, read_data_from_file(stk_data_path, study_name, "TotalBlackout"))
+    data_set['mean_blackout_time'] = sort_incremented_resolution_data(orbit_data, read_data_from_file(stk_data_path, study_name, "MeanBlackout"))
+    data_set['max_blackout_time'] = sort_incremented_resolution_data(orbit_data, read_data_from_file(stk_data_path, study_name, "MaxBlackout"))
 
-    # Import lunar target data
-    target_lighting = parse_csv_to_array("{}/DVP_{}_Target_Lighting.csv".format(stk_data_path, study_name), start)
-    target_eclipse = invert_events_list(target_lighting, total_duration)
-
-    # Cycle through unique perigees within data set, with apogee fixed at max value as defined above
-    for i in unique_perigees:
-        # Import sps data for orbit
-        sps_access = parse_csv_to_array("{}/DVP_{}_{}perigee{}apogee_access.csv".format(stk_data_path, study_name, i, max_apogee + r_moon), start)
-        sps_lighting = parse_csv_to_array("{}/DVP_{}_{}perigee{}apogee_lighting.csv".format(stk_data_path, study_name, i, max_apogee + r_moon), start)
-        # Determine active and blackout times
-        sps_active = determine_SPS_active_time(sps_lighting, target_eclipse, sps_access)
-        target_blackout = determine_blackout_data(sps_active, target_eclipse, total_duration)
-        # Extract blackout statistics
-        max_blackouts.append(np.max(target_blackout[2]))
-        mean_blackouts.append(np.mean(target_blackout[2]))
+    # Get max blackout data and mean blackout data strips nearest selected perigee altitude
+    g = min(enumerate(perigee_altitudes), key=lambda x: abs(x[1] - perigee))
+    max_blackouts = data_set['max_blackout_time'][:][g[0]]
+    mean_blackouts = data_set['mean_blackout_time'][:][g[0]]
+    total_blackouts = data_set['total_blackout_time'][:][g[0]]
 
     plt.figure(1)
-    plt.subplot(211)
-    plt.plot([j - r_moon for j in unique_perigees], [i / 3600.0 for i in max_blackouts])
+    plt.subplot(311)
+    plt.plot(apogee_altitudes, [i / 3600.0 for i in max_blackouts])
     plt.title('Max Blackout Durations')
-    plt.ylabel('Duration [hrs]')
-    plt.subplot(212)
-    plt.plot([j - r_moon for j in unique_perigees], [i / 3600.0 for i in mean_blackouts])
+    plt.ylabel('Hours')
+    plt.subplot(312)
+    plt.plot(apogee_altitudes, [i / 3600.0 for i in mean_blackouts])
     plt.title('Mean Blackout Durations')
+    plt.ylabel('Hours')
+    plt.subplot(313)
+    plt.plot(apogee_altitudes, [100.0 * i / total_duration for i in total_blackouts])
+    plt.title('Total Blackout Time')
+    plt.ylabel('Percent')
     plt.xlabel('Perigee Altitude')
     plt.show()
 
--- 007-DVP-SPS-Power-Storage/SPS_With_Power_Storage.py
+++ 007-DVP-SPS-Power-Storage/SPS_With_Power_Storage.py
@@ -10,54 +10,77 @@ for equatorial high altitude orbit, and polar lower altitude orbit.
 """
 
 from DVP_general_SPS_functions import *
+from DVP_Programmatic_Functions import *
+import os
 
 
 def main():
 
-    # Select orbit type which is studied. Options are Equatorial or Polar
-    orbit = 'Polar'
+    # Select which data set to access:
+    study_name = 'SouthPole_IncrementedRes_Inertial'
+    # study_name = 'Equatorial_IncrementedRes'
+
+    # Set bounds on parametric scan
+    max_perigee = 5000.0
+    max_apogee = 5000.0
+
+    # Get orbit data set
+    semi_maj_axis, eccentricity, orbit_data = vary_orbital_elements_incrementing_resolution(max_perigee, max_apogee)
 
     # Initialize simulation times, and refer to altitude corresponding to orbit selection
-    if orbit == 'Equatorial':
-        altitude = 30
-        start = convert_string_to_datetime(['2008', '07', '01', '10', '0', '0.0'])
-        end = convert_string_to_datetime(['2010', '06', '30', '10', '0', '0.0'])
-        total_duration = (end - start).total_seconds()
+    start = convert_string_to_datetime(['2018', '05', '17', '10', '0', '0.0'])
+    end = convert_string_to_datetime(['2020', '05', '17', '10', '0', '0.0'])
+    total_duration = (end - start).total_seconds()
 
-    elif orbit == 'Polar':
-        altitude = 3.5
-        start = convert_string_to_datetime(['2018', '05', '18', '10', '0', '0.0'])
-        end = convert_string_to_datetime(['2020', '05', '18', '10', '0', '0.0'])
-        total_duration = (end - start).total_seconds()
+    # Get pathway to main Lunar_SPS directory
+    current_folder = os.getcwd()
+    main_directory = os.path.dirname(current_folder)
 
-    # Import SPS illumination events
-    sps_lighting_raw = '{}/SPS({}k)_Lighting.csv'.format(orbit, altitude)
-    sps_lighting = parse_csv_to_array(sps_lighting_raw, start)
-    sps_eclipse = invert_events_list(sps_lighting, total_duration)
+    # Set file path for data
+    stk_data_path = r'{}\STK Data\{}'.format(main_directory, study_name)
 
-    # Import access data for sps
-    raw_access = "{}/SPS({}k)_Access.csv".format(orbit, altitude)
-    sps_access = parse_csv_to_array(raw_access, start)
-
-    # Import target lighting data, and invert to find eclipse events
-    raw_target_lighting = "{}/Target_Lighting.csv".format(orbit)
-    target_lighting = parse_csv_to_array(raw_target_lighting, start)
-    target_eclipse = invert_events_list(target_lighting, total_duration)
-
-    print('Total time spent in black-out w/o SPS: {}% per year'.format(
-        round(100.0 * np.sum(target_eclipse[2]) / total_duration, 2)))
-
-    stored_power_events = determine_SPS_storedpower_time(sps_eclipse, target_eclipse, sps_access)
-
-    print('Total time available for using stored power for WPT: {}% per year'.format(round(100.0 * np.sum(stored_power_events[2]) / total_duration, 2)))
+    max_stored_power_time = sort_incremented_resolution_data(orbit_data, read_data_from_file(stk_data_path, study_name, 'MaxStoredPowerEvent'))
+    total_stored_power_time = sort_incremented_resolution_data(orbit_data, read_data_from_file(stk_data_path, study_name, 'TotalStoredPowerEvent'))
 
     # Estimate battery size
     trans_power = 100e3
+    trans_eff = 0.35
     li_ion_energy_density = 270.0  # Watt hours per kilogram
-    battery_capacity = trans_power * max(stored_power_events[2]) / 3600.0  # in Watt hours
-    battery_mass = battery_capacity / li_ion_energy_density
+    fuel_cell_energy_density = 500.0
+    battery_mass = [trans_power * i / (trans_eff * 3600.0 * li_ion_energy_density) for i in max_stored_power_time]
+    fuel_cell_mass = [trans_power * i / (trans_eff * 3600.0 * fuel_cell_energy_density) for i in max_stored_power_time]
 
-    print("To power a {} kW transmitter for the longest event where store power is required ({} hours), a {} kg Li-ion battery is required".format(round(trans_power / 1000.0, 2), round(max(stored_power_events[2]) / 3600.0, 2), round(battery_mass, 2)))
+    # Extract unique perigees and apogees tested for plotting
+    unique_perigees = [orbit_data[1][0]]
+    unique_apogees = [orbit_data[1][1]]
+    r_moon = 1737.0
+    for i in range(1, len(orbit_data)):
+        if orbit_data[i][0] > max(unique_perigees):
+            unique_perigees.append(orbit_data[i][0])
+        if orbit_data[i][1] > max(unique_apogees):
+            unique_apogees.append(orbit_data[i][1])
+
+    # Reduce perigee and apogee to altitudes instead of radii
+    perigee_altitudes = [i - r_moon for i in unique_perigees]
+    apogee_altitudes = [i - r_moon for i in unique_apogees]
+
+    plt.subplot(131)
+    plt.contourf(apogee_altitudes, perigee_altitudes, total_stored_power_time / 3600.0, 500)
+    plt.xlabel('Apogee Altitude [km]')
+    plt.ylabel('Perigee Altitude [km]')
+    plt.colorbar()
+    plt.title('Total Stored Power Time [hrs]')
+    plt.subplot(132)
+    plt.contourf(apogee_altitudes, perigee_altitudes, battery_mass, 500)
+    plt.xlabel('Apogee Altitude [km]')
+    plt.colorbar()
+    plt.title('LiPo Battery Mass Reqd for Longest Event [kg]')
+    plt.subplot(133)
+    plt.contourf(apogee_altitudes, perigee_altitudes, fuel_cell_mass, 500)
+    plt.xlabel('Apogee Altitude [km]')
+    plt.colorbar()
+    plt.title('Fuel Cell Mass Reqd for Longest Event [kg]')
+    plt.show()
 
 
 main()
