1f44b4d259db3f46ebed01457b081bad86906297
Darian van Paridan <dvanpar@gmail.com>
2018-07-23 17:30:26 +0200

    #12 Updated equatorial data processing to account for non events

--- 008-DVP-STK-Programmatic-Scripts/Equatorial Config Many SPS/Process_Multi_SPS_Equatorial_Orbit_Data.py
+++ 008-DVP-STK-Programmatic-Scripts/Equatorial Config Many SPS/Process_Multi_SPS_Equatorial_Orbit_Data.py
@@ -101,41 +101,86 @@ def main():
             write_data_files(stk_data_path, study_name, data)
             sys.exit()
 
-        # Determine the total and maximum SPS active durations
-        sps_active_one = determine_SPS_active_time(sps_lighting_one, target_eclipse, sps_access_one)
-        sps_active_two = determine_SPS_active_time(sps_lighting_two, target_eclipse, sps_access_two)
-        sps_active = combine_events(sps_active_one, sps_active_two)
-        data['total_active_time'].append(np.sum(sps_active[2]))
-        data['max_active_time'].append(max(sps_active[2]))
-        data['mean_active_time'].append(np.mean(sps_active[2]))
-        
-        # Determine station-keeping/battery-charging events
-        sps_station_keeping_events_two = determine_battery_chargeup_events(sps_lighting_two, sps_access_two, total_duration)
-        data['total_station_keeping'].append(np.sum(sps_station_keeping_events_two[2]))
+        # If no access periods exist, insert nan into each list - infeasible orbit design
+        if not hasattr(sps_access_one[0], "__len__") or not hasattr(sps_access_two[0], "__len__"):
+            data['total_active_time'].append(np.nan)
+            data['max_active_time'].append(np.nan)
+            data['mean_active_time'].append(np.nan)
 
-        # Determine the total and maximum target blackout durations
-        target_blackout = determine_blackout_data(sps_active, target_eclipse, total_duration)
-        data['total_blackout_time'].append(np.sum(target_blackout[2]))
-        data['max_blackout_time'].append(max(target_blackout[2]))
-        data['mean_blackout_time'].append(np.mean(target_blackout[2]))
+            # Blackout equivalent to total eclipse in this case
+            data['total_blackout_time'].append(np.sum(target_eclipse[2]))
+            data['max_blackout_time'].append(max(target_eclipse[2]))
+            data['mean_blackout_time'].append(np.mean(target_eclipse[2]))
 
-        # Determine the mean range, averaging mean range per access event in time
-        try:
-            sps_range = import_range_data_statistics('DVP_{}_{}perigee{}apogee_180.0argperi_range'.format(study_name, orbit_data[i][0], orbit_data[i][1]), stk_data_path)
-        except IOError:
-            print('{}\DVP_{}_{}perigee{}apogee_180.0meananom_range.txt'.format(stk_data_path, study_name, orbit_data[i][0], orbit_data[i][1]))
-            write_data_files(stk_data_path, study_name, data)
-            sys.exit()
-        data['mean_range'].append(np.sum([(i * j) / np.sum(sps_access_two[2]) for i, j in zip(sps_range[2], sps_access_two[2])]))
-        data['mean_min_range'].append(np.sum([(i * j) / np.sum(sps_access_two[2]) for i, j in zip(sps_range[0], sps_access_two[2])]))
-        data['mean_max_range'].append(np.sum([(i * j) / np.sum(sps_access_two[2]) for i, j in zip(sps_range[1], sps_access_two[2])]))
+            data['mean_range'].append(np.nan)
+            data['mean_min_range'].append(np.nan)
+            data['mean_max_range'].append(np.nan)
 
-        # Determine events when SPS is in range but eclipsed
-        sps_eclipse = invert_events_list(sps_lighting_two, total_duration)
-        sps_use_stored_power = determine_SPS_storedpower_time(sps_eclipse, target_eclipse, sps_access_two)
-        data['total_stored_power_time'].append(np.sum(sps_use_stored_power[2]))
-        data['max_stored_power_time'].append(max(sps_use_stored_power[2]))
-        data['mean_stored_power_time'].append(np.mean(sps_use_stored_power[2]))
+            data['total_stored_power_time'].append(np.nan)
+            data['max_stored_power_time'].append(np.nan)
+            data['mean_stored_power_time'].append(np.nan)
+
+            data['total_station_keeping'].append(np.nan)
+
+        # Otherwise determine active periods
+        else:
+            # Calculate individual and combined active durations
+            sps_active_one = determine_SPS_active_time(sps_lighting_one, target_eclipse, sps_access_one)
+            sps_active_two = determine_SPS_active_time(sps_lighting_two, target_eclipse, sps_access_two)
+            sps_active = combine_events(sps_active_one, sps_active_two)
+
+            # IF no active period, put nan
+            if np.sum(sps_active) == 0.0:
+                data['total_active_time'].append(np.nan)
+                data['max_active_time'].append(np.nan)
+                data['mean_active_time'].append(np.nan)
+                data['mean_range'].append(np.nan)
+                data['mean_min_range'].append(np.nan)
+                data['mean_max_range'].append(np.nan)
+                # Blackout equivalent to total eclipse in this case
+                data['total_blackout_time'].append(np.sum(target_eclipse[2]))
+                data['max_blackout_time'].append(max(target_eclipse[2]))
+                data['mean_blackout_time'].append(np.mean(target_eclipse[2]))
+                data['total_station_keeping'].append(np.nan)
+
+            # Otherwise calculate blackout period
+            else:
+                data['total_active_time'].append(np.sum(sps_active[2]))
+                data['max_active_time'].append(max(sps_active[2]))
+                data['mean_active_time'].append(np.mean(sps_active[2]))
+
+                # Determine the total and maximum target blackout durations
+                target_blackout = determine_blackout_data(sps_active, target_eclipse, total_duration)
+                data['total_blackout_time'].append(np.sum(target_blackout[2]))
+                data['max_blackout_time'].append(max(target_blackout[2]))
+                data['mean_blackout_time'].append(np.mean(target_blackout[2]))
+
+                # Determine the mean range, averaging mean range per access event in time
+                try:
+                    sps_range = import_range_data_statistics('DVP_{}_{}perigee{}apogee_180.0argperi_range'.format(study_name, orbit_data[i][0], orbit_data[i][1]), stk_data_path)
+                except IOError:
+                    print('{}\DVP_{}_{}perigee{}apogee_180.0meananom_range.txt'.format(stk_data_path, study_name, orbit_data[i][0], orbit_data[i][1]))
+                    write_data_files(stk_data_path, study_name, data)
+                    sys.exit()
+                data['mean_range'].append(np.sum([(i * j) / np.sum(sps_access_two[2]) for i, j in zip(sps_range[2], sps_access_two[2])]))
+                data['mean_min_range'].append(np.sum([(i * j) / np.sum(sps_access_two[2]) for i, j in zip(sps_range[0], sps_access_two[2])]))
+                data['mean_max_range'].append( np.sum([(i * j) / np.sum(sps_access_two[2]) for i, j in zip(sps_range[1], sps_access_two[2])]))
+
+                # Determine station-keeping/battery-charging events
+                sps_station_keeping_events_two = determine_battery_chargeup_events(sps_lighting_two, sps_access_two, total_duration)
+                data['total_station_keeping'].append(np.sum(sps_station_keeping_events_two[2]))
+
+            # Determine events when SPS is in range but eclipsed
+            sps_eclipse = invert_events_list(sps_lighting_two, total_duration)
+            sps_use_stored_power = determine_SPS_storedpower_time(sps_eclipse, target_eclipse, sps_access_two)
+            if np.sum(sps_use_stored_power) == 0.0:
+                data['total_stored_power_time'].append(np.nan)
+                data['max_stored_power_time'].append(np.nan)
+                data['mean_stored_power_time'].append(np.nan)
+            else:
+                data['total_stored_power_time'].append(np.sum(sps_use_stored_power[2]))
+                data['max_stored_power_time'].append(max(sps_use_stored_power[2]))
+                data['mean_stored_power_time'].append(np.mean(sps_use_stored_power[2]))
 
     write_data_files(stk_data_path, study_name, data)
     ####################################################################################################################
