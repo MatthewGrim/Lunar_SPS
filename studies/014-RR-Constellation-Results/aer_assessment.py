78988f01651ea2d4cffc23e4b0b3cfec9cab4221
Rohan Ramasamy <Rohan@dhcp-10-176-41-79.estec.esa.int>
2018-11-12 16:38:56 +0100

    #42 - Committed additional data for the targets perspective

--- studies/014-RR-Constellation-Results/aer_assessment.py
+++ studies/014-RR-Constellation-Results/aer_assessment.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 from Lunar_SPS.pysrc.post_process_functions.DVP_general_SPS_functions import convert_string_to_datetime
 
 
-def read_data(fname, start):
+def read_data(fname, start, is_sat=True):
     f = open(fname)
     lines = f.readlines()
     data = np.zeros((8, len(lines) - 2))
@@ -28,20 +28,19 @@ def read_data(fname, start):
         # Orbital elements - sma, e, i, raan, peri, true anomaly, mean anomaly
         data[1, i - 1] = float(components[1])
         data[2, i - 1] = float(components[2])
-        data[3, i - 1] = float(components[3])
-        data[4, i - 1] = float(components[4])
-        data[5, i - 1] = float(components[5])
-        data[6, i - 1] = float(components[6])
+        if is_sat:
+	        data[3, i - 1] = float(components[3])
+	        data[4, i - 1] = float(components[4])
+	        data[5, i - 1] = float(components[5])
+	        data[6, i - 1] = float(components[6])
 
     return data
         
 
-def plot_aer():
-    start = convert_string_to_datetime(['2018', '05', '17', '10', '0', '0.0'])
-    main_dir = "AER_Validation"
+def plot_aer_sat_data():
     file = "aer_report.csv"
 
-    fig, ax = plt.subplots(2, 3, figsize=(15, 6))
+    fig, ax = plt.subplots(2, 3, figsize=(15, 6), sharex='col')
     path = os.path.join(main_dir, file)
     assert os.path.exists(path), path
 
@@ -55,11 +54,18 @@ def plot_aer():
     ax[1, 2].scatter(data[0, :], data[6, :], label='range rate')
 
     ax[0, 0].set_xlim([0.0, 1.5])
+    ax[0, 0].set_xlim([0.0, 1.5])
     ax[0, 1].set_xlim([0.0, 1.5])
     ax[0, 2].set_xlim([0.0, 1.5])
     ax[1, 0].set_xlim([0.0, 1.5])
     ax[1, 1].set_xlim([0.0, 1.5])
     ax[1, 2].set_xlim([0.0, 1.5])
+    ax[0, 0].set_ylim([np.min(data[1, :]), np.max(data[1, :])])
+    ax[0, 1].set_ylim([np.min(data[2, :]), np.max(data[2, :])])
+    ax[0, 2].set_ylim([np.min(data[3, :]), np.max(data[3, :])])
+    ax[1, 0].set_ylim([np.min(data[4, :]), np.max(data[4, :])])
+    ax[1, 1].set_ylim([np.min(data[5, :]), np.max(data[5, :])])
+    ax[1, 2].set_ylim([np.min(data[6, :]), np.max(data[6, :])])
     
     ax[0, 0].set_ylabel("Azimuth [$^\circ$]")
     ax[0, 1].set_ylabel("Elevation [$^\circ$]")
@@ -67,12 +73,43 @@ def plot_aer():
     ax[1, 0].set_ylabel("Azimuth [$^\circ s^{-1}$]")
     ax[1, 1].set_ylabel("Elevation [$^\circ s^{-1}$]")
     ax[1, 2].set_ylabel("Range [$kms^{-1}$]")
+    ax[1, 0].set_xlabel("Days]")
+    ax[1, 1].set_xlabel("Days]")
+    ax[1, 2].set_xlabel("Days]")
 
     plt.tight_layout()
-    plt.savefig(os.path.join(main_dir, "AER_Results"))
+    plt.savefig(os.path.join(main_dir, "AER_satellite_results"))
+    plt.show()
+    plt.close()
+
+def plot_aer_target():
+    file = "target_ae_report.csv"
+
+    fig, ax = plt.subplots(2, figsize=(15, 6), sharex='col')
+    path = os.path.join(main_dir, file)
+    assert os.path.exists(path), path
+
+    data = read_data(path, start, is_sat=False)
+    data[0, :] /= 86400
+    ax[0].scatter(data[0, :], data[1, :], label='azimuth')
+    ax[1].scatter(data[0, :], data[2, :], label='elevation')
+
+    ax[0].set_xlim([0.0, 1.5])
+    ax[1].set_xlim([0.0, 1.5])
+    ax[0].set_ylim([np.min(data[1, :]), np.max(data[1, :])])
+    ax[1].set_ylim([np.min(data[2, :]), np.max(data[2, :])])
+    
+    ax[0].set_ylabel("Azimuth [$^\circ$]")
+    ax[1].set_ylabel("Elevation [$^\circ$]")
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(main_dir, "AER_target_results"))
     plt.show()
     plt.close()
 
 
 if __name__ == '__main__':
-    plot_aer()
+    start = convert_string_to_datetime(['2018', '05', '17', '10', '0', '0.0'])
+    main_dir = "AER_Validation"
+    plot_aer_sat_data()
+    plot_aer_target()
